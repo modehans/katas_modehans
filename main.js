520a9de995701c6d70a727c830118019b4c93bb0
modehans <monicaglezsanch@gmail.com>
2022-11-09 17:59:41 +0100

    Add function 6kyu Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eurekagit add -A

--- main.js
+++ main.js
@@ -54,3 +54,22 @@ console.log(
     [121, 14641, 20736, 361, 25921, 361, 20736, 361]
   )
 );
+
+//Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
+const sumDigPow = (a, b) => {
+  let arr = [];
+  for (let i = a; i < b; i++) {
+    if (
+      i ==
+      i
+        .toString()
+        .split('')
+        .reduce((acc, b, i) => acc + b ** (i + 1), 0)
+    ) {
+      arr.push(i);
+    }
+  }
+  return arr;
+};
+
+console.log('sunDigPow', sumDigPow(5, 100));
