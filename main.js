d5f22df13597beac07b404598fa0d0f09d8a36ae
modehans <monicaglezsanch@gmail.com>
2022-11-09 17:22:50 +0100

    Add Is pangram string function

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ const factorial = (n) => {
   return n * factorial(n - 1);
 };
 console.log(factorial(6));
+
+// Detect Pangram
+const isPangram = (string) => {
+  return string.match(/([a-z])(?!.*\1)/gi).length === 26;
+};
+
+console.log(
+  'No es un pangram',
+  isPangram('Esto no es un pangram, ni se le parece')
+);
+console.log('Si es un pangram', isPangram('abcd efghijk lmno pqrs tuvwxyz'));
