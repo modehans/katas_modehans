79c35d437ec4a0296949dc258be87b0923ecbb44
modehans <monicaglezsanch@gmail.com>
2022-11-09 18:39:22 +0100

    Counting duplicates codewars

--- main.js
+++ main.js
@@ -99,3 +99,16 @@ const duplicateEncode = (word) => {
 };
 
 console.log('Duplicate encode', duplicateEncode('Success'));
+
+//Counting duplicates
+const duplicateCount = (text) =>
+  (
+    text
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('')
+      .match(/([^])\1+/g) || []
+  ).length;
+
+console.log('Letters duplicate count', duplicateCount('indivisibility'));
