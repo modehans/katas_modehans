d32b55bab159356576fe8a2364258fbfc86bee87
modehans <monicaglezsanch@gmail.com>
2022-11-09 17:11:00 +0100

    Add Factorial Function

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//Crear Factorial de un número con una función recurrente.
+const factorial = (n) => {
+  if (n === 1 || 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+};
+console.log(factorial(6));
