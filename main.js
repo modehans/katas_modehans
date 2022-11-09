346347767a9f7b6df63a157b19ba9f22134c9517
modehans <monicaglezsanch@gmail.com>
2022-11-09 18:49:10 +0100

    Add function likes count codewars

--- main.js
+++ main.js
@@ -136,3 +136,26 @@ console.log(
   'Moving Zeros To The End',
   moveZeros([false, 1, 0, 1, 2, 0, 1, 3, 'a'])
 );
+
+//Who likes it?
+const likes = (names) => {
+  switch (names.length) {
+    case 0:
+      return 'no one likes this';
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${
+        names.length - 2
+      } others like this`;
+  }
+};
+
+console.log(
+  'Likes count',
+  likes(['Alex', 'Ana', 'Jacob', 'Mark', 'Max', 'Susana'])
+);
