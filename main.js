4c26e5f38f55d06a59ab0c9e45075bd34604bea7
modehans <monicaglezsanch@gmail.com>
2022-12-07 16:52:41 +0100

    //Reto #4 del #AdventJS 2022

--- main.js
+++ main.js
@@ -537,3 +537,36 @@ function distributeGifts(packOfGifts, reindeers) {
   return ~~(weightReindeers / weightGifts);
 }
 console.log(distributeGifts(packOfGifts, reindeers));
+
+//Reto #4 del #AdventJS 2022
+function fitsInOneBox(boxes) {
+  let sortBoxes = boxes.sort((a, b) => {
+    if (a.l > b.l && a.w > b.w && a.h > b.h) {
+      return 1;
+    }
+    if (a.l < b.l && a.w < b.w && a.h < b.h) {
+      return -1;
+    }
+    return 0;
+  });
+  return sortBoxes.every((box, index) => {
+    if (index === 0) return true;
+    const prev = boxes[index - 1];
+    return box.l > prev.l && box.w > prev.w && box.h > prev.h;
+  });
+}
+
+const boxes1 = [
+  { l: 1, w: 1, h: 1 },
+  { l: 3, w: 3, h: 3 },
+  { l: 2, w: 2, h: 2 },
+];
+
+const boxes2 = [
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 },
+  { l: 3, w: 1, h: 3 },
+];
+
+console.log('priemro', fitsInOneBox(boxes1));
+console.log('segundo', fitsInOneBox(boxes2));
